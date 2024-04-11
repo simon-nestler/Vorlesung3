bd59bdd109ce8bb6c9c312a9c99681fb35eb261d
Simon Nestler <simon.nestler@thi.de>
2024-04-11 12:26:31 +0200

    Beispiel Folie 12

--- src/App.java
+++ src/App.java
@@ -5,10 +5,11 @@ public class App {
 
     public static void beispieleFuerPrintF() {
         String article = "Buch";
-        float price = 33.75f;
+        String article23 = "Buch23";
+        float price = 33.32499f;
 
-        System.out.println("Das " + article + " kostet " + price + " Euro");
-        System.out.printf("Das %s kostet %.2f Euro\n", article, price);
+        System.out.printf("Das %10s kostet %10.2f Euro\n", article, price);
+        System.out.printf("Das %10s kostet %10.2f Euro\n", article23, price);
     }
 
     public static void steuerZeichen() {
