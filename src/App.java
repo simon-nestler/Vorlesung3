6e7db29fa45830b517af1429800324e8ad3b5f3d
Simon Nestler <simon.nestler@thi.de>
2024-04-11 12:13:41 +0200

    Beispiel Folie 9

--- src/App.java
+++ src/App.java
@@ -1,10 +1,9 @@
-import java.util.Properties;
-
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("Max sagt:");
+        System.out.println("\t\"Hello, World!\"");
+        System.out.println("Lisa antwortet:\n\t\'Hallo Max!\'");
+        System.out.println("Meine Festplatte lautet: C:\\");
     }
 
-    public static void printProperties(Properties properties) {
-
-    }
 }
