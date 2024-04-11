cf591d3895def47870d68c0ebe2a5b33b0ff28ce
Simon Nestler <simon.nestler@thi.de>
2024-04-11 11:50:15 +0200

    java version

--- src/App.java
+++ src/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         Properties properties = new Properties(System.getProperties());
         System.out.println("Betriebssystem: " + properties.getProperty("os.name"));
-
+        System.out.println("Java-Version: " + properties.getProperty("java.version"));
     }
 }
