2b0cabed31daceb7c9b0fb6d8b394373984bff7c
Simon Nestler <simon.nestler@thi.de>
2024-04-11 12:17:22 +0200

    unterschied zwischen println und printf

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        beispieleFuerPrintF();
+    }
+
+    public static void beispieleFuerPrintF() {
+        String article = "Buch";
+        float price = 33.75f;
+
+        System.out.println("Das " + article + " kostet " + price + " Euro");
+        System.out.printf("Das %s kostet %.2f Euro\n", article, price);
+    }
+
+    public static void steuerZeichen() {
         System.out.println("Max sagt:");
         System.out.println("\t\"Hello, World!\"");
         System.out.println("Lisa antwortet:\n\t\'Hallo Max!\'");
